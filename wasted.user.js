0a83e1c504c8f3db815bf5ca9520b96e3677af82
Rohan Bhattacharyya <dev.creator42@gmail.com>
2025-06-20 10:36:25 -0700

    Setup

---
+++ wasted.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        New script mozilla.org
+// @namespace   Violentmonkey Scripts
+// @match       *youtube.com*
+// @match       *reddit.com*
+// @match       *facebook.com*
+// @match       *instagram.com*
+// @match       *x.com*
+
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 6/20/2025, 10:25:44 AM
+// ==/UserScript==
+
+
